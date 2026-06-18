101e6b2b78579a3718a8f0753305e4a155bfc86c
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-06-18 21:36:16 +0300

    perf: add loading skeleton to showcase

---
+++ apps/web/src/app/showcase/loading.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/ui/skeleton";
+export default function Loading() {
+  return <div className="container mx-auto max-w-4xl px-4 py-16 space-y-8">
+    <Skeleton className="h-8 w-64"/><Skeleton className="h-4 w-96"/>
+    <Skeleton className="h-32 w-full"/><Skeleton className="h-32 w-full"/>
+  </div>;
+}
