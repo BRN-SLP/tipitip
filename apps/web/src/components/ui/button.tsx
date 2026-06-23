33c2809573d3d92b81710edf6391c8b8e7949658
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-06-23 04:59:34 +0300

    a11y(editor): heading level navigation

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -54,3 +54,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = 'Button';
 
 export { Button, buttonVariants };
+// @cleanup: remove unused import on refactor
