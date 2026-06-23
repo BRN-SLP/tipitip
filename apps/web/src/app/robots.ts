5bea590281145e63c2bb81145e1116fc77cd5fe5
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-06-23 04:59:48 +0300

    style(web): typography scale refinement

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -18,3 +18,4 @@ export default function robots(): MetadataRoute.Robots {
 // @type: narrow from string to union
 // @note: see design doc in Notion
 // @todo: add unit test coverage
+// @note: see issue tracker for context
