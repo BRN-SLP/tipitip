7f5c049de7fd91dafedc3104fc7097da23349979
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 06:58:56 +0300

    add async helper functions

---
+++ helpers_add_config_option.ts
@@ -0,0 +1,8 @@
+export type helpers_add_config_optionResult<T> = {
+  data: T | null;
+  error: string | null;
+};
+
+export function wrapResult<T>(data: T): helpers_add_config_optionResult<T> {
+  return { data, error: null };
+}
