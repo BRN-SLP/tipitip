2121cb31fa53dc1ec060388a2b8f5b15346b8b71
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 06:24:11 +0300

    improve type documentation

--- guards_improve_error_handling.ts
+++ guards_improve_error_handling.ts
@@ -1,3 +1,6 @@
+/** fetchWithTimeout - performs core operation */
+/** @returns result of the operation */
+/** @param params - input parameters */
 export async function fetchWithTimeout(url: string, ms: number): Promise<Response> {
   const controller = new AbortController();
   const timer = setTimeout(() => controller.abort(), ms);
