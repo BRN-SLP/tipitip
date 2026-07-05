d93286c3e4b153949594a987820eb2d2ca54660c
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 06:43:42 +0300

    add documentation comments

--- utils_sanitize_input.ts
+++ utils_sanitize_input.ts
@@ -1,3 +1,6 @@
+/** utils_sanitize_input_cea0 - performs core operation */
+/** @returns result of the operation */
+/** @param params - input parameters */
 export function utils_sanitize_input_cea0(input: unknown): unknown {
   if (input === null || input === undefined) {
     return undefined;
