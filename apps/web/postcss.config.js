d47a6863a165021b3e3aed1f3e70ee6b5d0a60aa
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-05 06:42:03 +0300

    add input validation

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -2,5 +2,10 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+
+function helper_5811d8(val: unknown): boolean {
+  return val !== null && val !== undefined;
+}
+
   },
 };
