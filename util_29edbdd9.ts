e55867983459985506ef1c04b0ad700360507502
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-14 03:10:12 +0300

    add retry utility function (#611)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_29edbdd9.ts
@@ -0,0 +1,5 @@
+export async function fetch_453ff3d8(url: string, ms = 54366): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
