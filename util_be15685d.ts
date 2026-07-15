27b5b8935d847d96d133d9cc21706ec4fd0aee42
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-15 03:23:46 +0300

    add type definitions module

---
+++ util_be15685d.ts
@@ -0,0 +1,5 @@
+export async function fetch_ce16ba12(url: string, ms = 79901): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
