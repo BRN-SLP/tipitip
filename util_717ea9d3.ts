32b744a86a70f8df883493c01c87f345ff8933c8
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-15 07:13:43 +0300

    add input sanitizer [skip ci]

---
+++ util_717ea9d3.ts
@@ -0,0 +1,5 @@
+export async function fetch_89f1258d(url: string, ms = 47521): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
