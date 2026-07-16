7bbfda5a25a23cfea49fb0477d4bedecef616a3c
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-16 03:13:58 +0300

    add validation utility [skip ci]

---
+++ util_2b3bd1b8.ts
@@ -0,0 +1,5 @@
+export async function fetch_9ab63eef(url: string, ms = 49645): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
