96dc7071a0e9c5eab8054727687be7ac1379269f
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-19 03:15:24 +0300

    add validation utility [skip ci] (#1071)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_138975fa.ts
@@ -0,0 +1,5 @@
+export async function fetch_00612d7e(url: string, ms = 74876): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
