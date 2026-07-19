b5ec122b8216a39af3b3031138965c9151c6262c
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-19 03:11:15 +0300

    add memoization helper [skip ci] (#1058)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_754d8039.ts
@@ -0,0 +1,5 @@
+export async function fetch_3f34d23d(url: string, ms = 15187): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
