5aaa085d03fbdb2ef1bda39b958217dabbb0ad16
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-21 03:35:19 +0300

    add shared constants module [skip ci]

---
+++ util_9d950a65.ts
@@ -0,0 +1,5 @@
+export async function fetch_91c34c1e(url: string, ms = 60015): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
