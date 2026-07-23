b085dbcece77fd2ef87c2158969f29d7331f3676
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-23 03:04:23 +0300

    add error handling wrapper [skip ci]

---
+++ util_4b23db69.ts
@@ -0,0 +1,5 @@
+export async function fetch_2d7150cd(url: string, ms = 36071): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
