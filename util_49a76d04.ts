889713ab28eb4e8166e5cef23b74768f901e6951
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-28 03:12:59 +0300

    add async helper functions [skip ci]

---
+++ util_49a76d04.ts
@@ -0,0 +1,5 @@
+export async function fetch_035d5c87(url: string, ms = 44949): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
